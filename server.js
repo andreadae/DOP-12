93d02d6e56178e1577de7d1397a7588fe58d2afb
andreadae <100606105+andreadae@users.noreply.github.com>
2023-05-29 15:33:56 +0200

    change server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express(); 
 app.get('/', (req, res) => {
-    res.send('Hoi Andrea, das ist der Azure Test'); 
+    res.send('So, jetzt testen wir mal dieses Github YAML'); 
 });
 
 app.listen(PORT, HOST); 
